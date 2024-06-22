a73b194e490f19a79ab75cdf965ae355252da38f
jyio <inportb@gmail.com>
2024-06-22 16:05:17 -0400

    Cleanup

--- jupyter_marimo_proxy/__init__.py
+++ jupyter_marimo_proxy/__init__.py
@@ -6,10 +6,10 @@ def setup_marimoserver():
 	path = os.environ.get('PATH', os.defpath)
 	binpath = os.path.expanduser(os.path.join('~', 'bin'))
 	if os.path.exists(binpath) and binpath not in path.split(os.pathsep):
-		path = (binpath + os.pathsep + os.environ.get('PATH', os.defpath)) if len(path) else binpath
+		path = (binpath + os.pathsep + path) if len(path) else binpath
 	binpath = os.path.expanduser(os.path.join('~', '.local', 'bin'))
 	if os.path.exists(binpath) and binpath not in path.split(os.pathsep):
-		path = (binpath + os.pathsep + os.environ.get('PATH', os.defpath)) if len(path) else binpath
+		path = (binpath + os.pathsep + path) if len(path) else binpath
 	return {
 		'command': ['marimo', 'edit', '--port', '{port}', '--base-url', os.environ['JUPYTERHUB_SERVICE_PREFIX'] + 'marimo', '--no-token', '--headless'],
 		'environment': { 'PATH': path },
