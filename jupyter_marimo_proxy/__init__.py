9337d0d2c8163f6b7cc3161bf3e58cee41eb1ed3
Olivier Benz <olivier.benz@b-data.ch>
2024-06-22 21:07:42 +0200

    Update PATH before executing marimo
    - Fix https://github.com/jyio/jupyter-marimo-proxy/issues/1
    - Fix https://github.com/jyio/jupyter-marimo-proxy/issues/3

--- jupyter_marimo_proxy/__init__.py
+++ jupyter_marimo_proxy/__init__.py
@@ -2,13 +2,45 @@
 
 import os
 
+if (
+	os.path.exists(os.path.join(os.environ["HOME"], "bin"))
+	and not os.path.join(os.environ["HOME"], "bin") in os.getenv("PATH", "")
+):
+	os.environ["PATH"] = (
+		os.path.join(os.environ["HOME"], "bin")
+		+ os.pathsep
+		+ os.getenv("PATH", "")
+	)
+
+if (
+	os.path.exists(os.path.join(os.environ["HOME"], ".local", "bin"))
+	and not os.path.join(os.environ["HOME"], ".local", "bin")
+	in os.getenv("PATH", "")
+):
+	os.environ["PATH"] = (
+		os.path.join(os.environ["HOME"], ".local", "bin")
+		+ os.pathsep
+		+ os.getenv("PATH", "")
+	)
+
 def setup_marimoserver():
-	return {
-		'command': ['marimo', 'edit', '--port', '{port}', '--base-url', '/user/' + os.environ['JUPYTERHUB_USER'] + '/marimo', '--no-token', '--headless'],
-		'timeout': 60,
-		'absolute_url': True,
-		'launcher_entry': {
-			'title': 'Marimo',
-			'icon_path': os.path.join(os.path.dirname(os.path.abspath(__file__)), 'icon.svg')
-		},
-	}
+    return {
+        "command": [
+            "marimo",
+            "edit",
+            "--port",
+            "{port}",
+            "--base-url",
+            os.environ["JUPYTERHUB_SERVICE_PREFIX"] + "marimo",
+            "--no-token",
+            "--headless",
+        ],
+        "timeout": 60,
+        "absolute_url": True,
+        "launcher_entry": {
+            "title": "Marimo",
+            "icon_path": os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), "icon.svg"
+            ),
+        },
+    }
