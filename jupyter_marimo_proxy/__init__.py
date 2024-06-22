72fa06015e1f73ab90ea72f1cd0ffc1e7ae531c9
jyio <inportb@gmail.com>
2024-06-22 15:54:17 -0400

    Prepend `~/bin` and `~/.local/bin` to `PATH` when starting Marimo

--- jupyter_marimo_proxy/__init__.py
+++ jupyter_marimo_proxy/__init__.py
@@ -3,8 +3,16 @@
 import os
 
 def setup_marimoserver():
+	path = os.environ.get('PATH', os.defpath)
+	binpath = os.path.expanduser(os.path.join('~', 'bin'))
+	if os.path.exists(binpath) and binpath not in path.split(os.pathsep):
+		path = (binpath + os.pathsep + os.environ.get('PATH', os.defpath)) if len(path) else binpath
+	binpath = os.path.expanduser(os.path.join('~', '.local', 'bin'))
+	if os.path.exists(binpath) and binpath not in path.split(os.pathsep):
+		path = (binpath + os.pathsep + os.environ.get('PATH', os.defpath)) if len(path) else binpath
 	return {
 		'command': ['marimo', 'edit', '--port', '{port}', '--base-url', os.environ['JUPYTERHUB_SERVICE_PREFIX'] + 'marimo', '--no-token', '--headless'],
+		'environment': { 'PATH': path },
 		'timeout': 60,
 		'absolute_url': True,
 		'launcher_entry': {
